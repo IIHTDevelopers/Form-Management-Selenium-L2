e870d65c0954c1be7823949ffde60452983758d4
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-03-26 11:23:34 +0530

    Update LocatorsFactory.java

--- src/test/java/testdata/LocatorsFactory.java
+++ src/test/java/testdata/LocatorsFactory.java
@@ -39,7 +39,17 @@ public class LocatorsFactory extends StartupPage {
 public boolean compareImages(BufferedImage img1, BufferedImage img2) {
         // Image comparison logic
         // Example: Compare image dimensions and pixel values
-        return img1.getWidth() == img2.getWidth() && img1.getHeight() == img2.getHeight();
+         if (img1.getWidth() == img2.getWidth() && img1.getHeight() == img2.getHeight()) {
+        for (int x = 0; x < img1.getWidth(); x++) {
+            for (int y = 0; y < img1.getHeight(); y++) {
+                if (img1.getRGB(x, y) != img2.getRGB(x, y))
+                    return false;
+            }
+        }
+    } else {
+        return false;
+    }
+    return true;
     }
 
 
